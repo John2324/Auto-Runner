0cb287fc4a30774a0ac4766f487c1d1d476f50d7
John <jmonte80@calstatela.edu>
2019-12-12 12:38:39 -0800

    Automate running character

--- js/player.js
+++ js/player.js
@@ -7,8 +7,9 @@ game.PlayerEntity = me.Entity.extend({
       this._super(me.Entity, 'init', [x, y, settings]);
   
       // max walking & jumping speed
-      this.body.setMaxVelocity(8, 18);
-      this.body.setFriction(0.5, 0);
+      this.body.setVelocity(8, 18);
+      this.body.setFriction(0.6, 0);
+
   
       // set the display to follow our position on both axis
       me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH, 0.5);
@@ -26,60 +27,35 @@ game.PlayerEntity = me.Entity.extend({
       this.renderable.addAnimation("jumping", [0, 1, 2, 3, 4, 5]);
   
       // set initial standing animation as default
-      this.renderable.setCurrentAnimation("start");
+      this.renderable.setCurrentAnimation("run");
     },
   
     /**
      * update the entity
      */
     update : function (dt) {
+      this.pos.x += 0.4*dt;
   
-        if (me.input.isKeyPressed('left')) {
-  
-            // flip the sprite on horizontal axis
-            this.renderable.flipX(true);
-            // update the default force
-            this.body.force.x = -this.body.maxVel.x;
-            // change to the walking animation
-            if (!this.renderable.isCurrentAnimation("run")) {
-                this.renderable.setCurrentAnimation("run");
-            }
-        } else if (me.input.isKeyPressed('right')) {
-  
-            // unflip the sprite
-            this.renderable.flipX(false);
-            // update the entity velocity
-            this.body.force.x = this.body.maxVel.x;
-            // change to the walking animation
-            if (!this.renderable.isCurrentAnimation("run")) {
-                this.renderable.setCurrentAnimation("run");
-            }            
-        } else {
-            this.body.force.x = 0;
-            // change to the standing animation
-            this.renderable.setCurrentAnimation("start");
-        }
-  
-        if (me.input.isKeyPressed('jump')) {
+      if (me.input.isKeyPressed('jump')) {
 
-          if (!this.body.jumping && !this.body.falling)
-          {
-              // set current vel to the maximum defined value
-              // gravity will then do the rest
-              this.body.force.y = -this.body.maxVel.y
-          }
+        if (!this.body.jumping && !this.body.falling)
+        {
+            // set current vel to the maximum defined value
+            // gravity will then do the rest
+            this.body.force.y = -this.body.maxVel.y
+        }
         } else {
           this.body.force.y = 0;
         }
-  
-        // apply physics to the body (this moves the entity)
-        this.body.update(dt);
-  
-        // handle collisions against other shapes
-        me.collision.check(this);
-  
-        // return true if we moved or if the renderable was updated
-        return (this._super(me.Entity, 'update', [dt]) || this.body.vel.x !== 0 || this.body.vel.y !== 0);
+
+      // apply physics to the body (this moves the entity)
+      this.body.update(dt);
+
+      // handle collisions against other shapes
+      me.collision.check(this);
+
+      // return true if we moved or if the renderable was updated
+      return (this._super(me.Entity, 'update', [dt]) || this.body.vel.x !== 0 || this.body.vel.y !== 0);
     },
   
     /**
@@ -91,3 +67,4 @@ game.PlayerEntity = me.Entity.extend({
       return true;
     }
   });
+  
